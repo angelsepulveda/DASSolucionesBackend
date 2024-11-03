5c3aa51506f59a379bff88f0ccfe953bd15ea489
Aangelsepulveda <angelsepulvedasep@gmail.com>
2024-11-02 22:57:25 -0300

    instalacion de serilog con docker

--- DaSSolucionesBackend.Api/Program.cs
+++ DaSSolucionesBackend.Api/Program.cs
@@ -1,8 +1,12 @@
 using Carter;
 using DASSolucionesBackend.Shared.Exceptions.Handler;
 using DASSolucionesBackend.Shared.Extensions;
+using Serilog;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, config) =>
+    config.ReadFrom.Configuration(context.Configuration));
 
 //se agrega los contenedores de servicios
 builder.Services.AddCarterWithAssemblies(typeof(GeneralModule).Assembly);
@@ -13,9 +17,8 @@ builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 WebApplication app = builder.Build();
 
 app.MapCarter();
+app.UseSerilogRequestLogging();
+app.UseExceptionHandler(options => { });
 
 app.UseGeneralModule();
-
-app.UseExceptionHandler(options => {});
-
 app.Run();
